ca36c7cae7824e82fa6a435edeb0bd74d94a0cda
VeRTeXR <p5vasut27665@gmail.com>
2018-02-21 22:48:00 +0700

    nice

---
+++ Leverage/Assets/Resources/Scripts/GameController.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class  GameController : MonoBehaviour
+{
+
+	public GameObject Timer;
+	public GameObject Player;
+	public float MaxTimer;
+	public float CurrentTimer;
+	private bool _playerCompletedTheLevel;
+	
+	void Awake()
+	{
+		if(Timer == null)
+			Timer = GameObject.Find("Timer");
+
+		InitializeTimer();
+	}
+
+	private void InitializeTimer()
+	{
+		MaxTimer = 10;
+		CurrentTimer = MaxTimer;
+	}
+
+	void Update ()
+	{
+		CurrentTimer = CurrentTimer - Time.deltaTime;
+		Timer.GetComponent<Text>().text = CurrentTimer.ToString("F3");
+
+		if (CurrentTimer <= 0)
+			GameOver();
+
+		if (_playerCompletedTheLevel)
+		{
+			LevelComplete();
+			Debug.LogError("increment level do shit");
+		} 
+	}
+
+	private void LevelComplete()
+	{
+		
+	}
+
+	private void GameOver()
+	{
+		Debug.LogError("do something on game over");
+		CurrentTimer = MaxTimer;
+	}
+}
---
+++ Leverage/Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private float _walkSpeed;
+	private int _faceDirection;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_walkSpeed = 1; 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		ProcessingInput();
+
+	}
+
+	void ProcessingInput()
+	{
+		if (Input.GetKey("w"))
+		{
+				
+		}
+		if (Input.GetKey("a"))
+		{
+			
+		}
+		if (Input.GetKey("s"))
+		{
+			
+		}
+		if (Input.GetKey("d"))
+		{
+			
+		}
+	} 
+}
