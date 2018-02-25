cb08b10b705fdc0af094dcb126a3dc1f0cd7c19e
VeRTeXR <p5vasut27665@gmail.com>
2018-02-25 17:01:07 +0700

    Player now rotate 90 degree and have interval on input processing

--- Leverage/Assets/Resources/Scripts/PlayerController.cs
+++ Leverage/Assets/Resources/Scripts/PlayerController.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Diagnostics;
+using UnityEditor;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class PlayerController : MonoBehaviour
 {
@@ -6,36 +9,59 @@ public class PlayerController : MonoBehaviour
 	private int _faceDirection;
 	private float _walkCountdownInterval;
 	private float _currentWalkCountdownInterval;
+	private float _keyDownInterval;
+	private float _currentKeyDownInterval;
+	private bool _hasPlayerInputBeenProcessed;
 	
 	
 	void Start ()
 	{
 		_walkSpeed = 10f;
 		_walkCountdownInterval = 1f;
+		_keyDownInterval = 0.25f;
+		_currentKeyDownInterval = _keyDownInterval;
+		_hasPlayerInputBeenProcessed = false;
 	}
 	
 	void Update ()
 	{
 		ProcessingInput();
-		_currentWalkCountdownInterval--;
+		MovePlayer();
+		
+		if (_hasPlayerInputBeenProcessed)
+			HandlePlayerInputCooldownInterval();
+		
+	}
 
-		if (_currentWalkCountdownInterval <= 0)
+	private void HandlePlayerInputCooldownInterval()
+	{
+		_currentKeyDownInterval -= Time.deltaTime;
+		if (_currentKeyDownInterval <= 0)
 		{
-			MovePlayer();
-			_currentWalkCountdownInterval = _walkCountdownInterval;
-		} 
+			_hasPlayerInputBeenProcessed = false;
+			_currentKeyDownInterval = _keyDownInterval;
+		}
 	}
 
 	private void MovePlayer()
 	{
-		transform.localPosition = transform.localPosition + transform.up * _walkSpeed * Time.deltaTime;
+		transform.localPosition = transform.localPosition + transform.right * _walkSpeed * Time.deltaTime;
 	}
 
 	void ProcessingInput()
 	{ 
+		if(_hasPlayerInputBeenProcessed) return;
+		
 		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
-			transform.Rotate(Vector3.forward * 90 * Time.deltaTime);
+		{
+			transform.Rotate(Vector3.forward * 90);
+			_hasPlayerInputBeenProcessed = true;
+		}
 		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
-			transform.Rotate(-Vector3.forward * 90 * Time.deltaTime);
+		{
+			transform.Rotate(-Vector3.forward * 90);
+			_hasPlayerInputBeenProcessed = true;
+		}
+
 	} 
 }
